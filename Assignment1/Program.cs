9cb43893d3173a3ee6224331811886498d7d6317
ahmedSenara <ahmedomar11345@gmail.com>
2025-09-20 17:08:30 +0300

    Add project files.

---
+++ Assignment1/Program.cs
@@ -0,0 +1,132 @@
+﻿namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //################## Assignment 1 ##################//
+
+            #region Question 1
+            //Console.Write("Please Enter The Number: ");
+            //int.TryParse(Console.ReadLine(), out int number);
+            //Console.WriteLine($"Your Number Is {number}"); 
+            #endregion
+
+            #region Question 2
+            //string s = "Ahmed";
+            //int.TryParse(s, out int number);
+            //Console.WriteLine(number); // Output: 0 
+            #endregion
+
+            #region Question 3
+            //float num1 = 10.5f;
+            //float num2 = 20.4f;
+            //float result = num1 * num2;
+            //Console.WriteLine(result); // Output: 214.2 
+            #endregion
+
+            #region Question 4
+            //string text = "Ahmed Omar";
+            //string subtext = text.Substring(6);
+            //Console.WriteLine($"Text is: {text}");
+            //Console.WriteLine($"Subtext is: {subtext}"); // Output: "Omar" 
+            #endregion
+
+            #region Question 5
+            //int x = 10;
+            //int y = 20;
+            //Console.WriteLine($"Before: x = {x}, y = {y}"); // Output: x = 10, y = 20
+            //y = x;
+            //x = 30;
+            //Console.WriteLine($"After: x = {x}, y = {y}"); // Output: x = 30, y = 10 
+            #endregion
+
+            #region Question 6
+            //Student s1 = new Student();
+            //Student s2 = new Student();
+
+            //s1.id = 10;
+            //s2.id = 20;
+            //Console.WriteLine($"Before: s1 ID: {s1.id}, s2 ID: {s2.id}"); // Output: s1 ID: 10, s2 ID: 20
+
+            //s2 = s1;
+            //s1.id = 30;
+            //Console.WriteLine($"After: s1 ID: {s1.id}, s2 ID: {s2.id}"); // Output: s1 ID: 30, s2 ID: 30 
+            #endregion
+
+            #region Question 7
+            //Console.Write("Please Enter Your First Name: ");
+            //string? firstName = Console.ReadLine();
+            //Console.Write("Please Enter Your Last Name: ");
+            //string? lastName = Console.ReadLine();
+
+            //string fullName = firstName + " " + lastName;
+            //Console.WriteLine($"Your Full Name Is: {fullName}"); 
+            #endregion
+
+            #region QUestion 8
+            //Console.WriteLine("Program To Calculates The Simple Interest.");
+            //Console.WriteLine("------------------------------------------");
+            //Console.Write("Please Enter The Principal Amount: ");
+            //decimal.TryParse(Console.ReadLine(), out decimal principal);
+            //Console.Write("Please Enter The Rate Of Interest: ");
+            //decimal.TryParse(Console.ReadLine(), out decimal rate);
+            //Console.Write("Please Enter The Time: ");
+            //decimal.TryParse(Console.ReadLine(), out decimal time);
+            //decimal result = (principal * rate * time) / 100;
+            //Console.WriteLine($"The Simple Interest Is: {result}"); 
+            #endregion
+
+            #region Question 9
+            //Console.WriteLine("Program That Calculates The Body Mass Index (BMI).");
+            //Console.WriteLine("--------------------------------------------------");
+            //Console.Write("Please Enter Your Weight (in kg): ");
+            //decimal.TryParse(Console.ReadLine(), out decimal weight);
+            //Console.Write("Please Enter Your Height (in meters): ");
+            //decimal.TryParse(Console.ReadLine(), out decimal height);
+            //decimal bmi = (weight) / (height * height);
+            //Console.WriteLine($"Your Body Mass Index (BMI) Is: {bmi}"); 
+            #endregion
+
+            #region Question 10
+            //Console.WriteLine("This Program is Check If The Temperature Is Too Hot, Too Cold or Just Good.");
+            //Console.WriteLine("---------------------------------------------------------------------------");
+            //Console.Write("Please Enter The Temperature: ");
+            //decimal.TryParse(Console.ReadLine(), out decimal temp);
+
+            //string result = (temp < 10) ? "Just Cold" : (temp > 30) ? "Just Hot" : "Just Good";
+            //Console.WriteLine($"The Temperature Is: {result}"); 
+            #endregion
+
+            #region Question 11
+            //Console.WriteLine("Displays Date In Verious Formats.");
+            //Console.WriteLine("---------------------------------");
+            //Console.Write("Please Enter The Date: ");
+            //DateTime.TryParse(Console.ReadLine(), out DateTime dateInput);
+
+            //Console.WriteLine($"Today's date: {dateInput:dd,MM,yyyy}");
+            //Console.WriteLine($"Today's date: {dateInput:dd/MM/yyyy}");
+            //Console.WriteLine($"Today's date: {dateInput:dd-MM-yyyy}"); 
+            #endregion
+
+            #region Question 12
+            // The Answer Is: (c) The event is on 06/14/2024
+            #endregion
+
+            #region Question 13
+            // The Answer Is: (f) A value 1 will be assigned to d. // Because the expression (30 < 20) evaluates to false, and !(false) => true = 1
+
+            #endregion
+
+            #region Question 14
+            // The Answer Is: (d) 6 1
+
+            #endregion
+
+            #region Question 15
+            // The Answer Is: (d) 7 7
+            #endregion
+
+        }
+    }
+}
